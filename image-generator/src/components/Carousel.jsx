325b56502cfd7832fed3943be3b189e42e1c6b0d
inno <narendrapanchal020@gmail.com>
2024-10-07 17:41:38 +0530

    as

--- image-generator/src/components/Carousel.jsx
+++ image-generator/src/components/Carousel.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-function Carousel({images}) {
+function Carousel({ images, interval }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState('');
+  const containerRef = React.useRef(null);
+  useEffect(()=>{
+  },[])
+  const handleNext = () => {
+    const imageWidth = containerRef.current; // Get the width of the container
+    console.log("imageWidth",imageWidth)
+      containerRef.current.scrollLeft = imageWidth;
+  };
+
+  const handlePrevious = () => {
+    const imageWidth = containerRef.current.style.width; // Get the width of the container
+    containerRef.current.scrollRight = imageWidth;
+  };
+
   return (
-    <div>
-      
+    <div className={`carousel ${direction}`}>
+      <div ref={containerRef}  style={{display:"flex", overflow:"scroll", flexWrap:"nowrap"}}>
+      {images.map((image,ind)=><img src={image}  alt={"Image "+ind} style={{width:'inherit'}}/>)}
+      </div>
+      <button onClick={handlePrevious} className='bg-white absolute left-1 top-1/2'>Prev</button>
+      <button onClick={handleNext} className='bg-white absolute right-1 top-1/2'>Next</button>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- image-generator/src/pages/Home.jsx
+++ image-generator/src/pages/Home.jsx
@@ -1,16 +1,47 @@
-import React, { useEffect, useState } from 'react'
+const data=[
+  "https://unsplash.com/photos/9uMvphEhd_c/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHwxfHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/wxHCu_Ri_MY/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHwyfHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/IPRFX7CVVoU/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHwzfHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/xnyfMfMM6Fk/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHw0fHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/PFebkF2EnL8/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHw1fHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/jQ4u59JuqAY/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHw2fHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/AgYJdcEZtmY/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHw3fHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/T_iuKuVlDv0/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHw4fHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/s05bltSqfYs/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHw5fHxjYXR8ZW58MHwxfHxncmVlbnwxNzI4Mjc0NTEwfDA",
+  "https://unsplash.com/photos/G0I6VfStU6A/download?ixid=M3w2NjE5Nzh8MHwxfHNlYXJjaHwxMHx8Y2F0fGVufDB8MXx8Z3JlZW58MTcyODI3NDUxMHww"
+]
 
+import React, { useEffect, useState } from 'react'
+import { createApi } from 'unsplash-js';
+import Carousel from '../components/Carousel';
+const unsplash = createApi({ accessKey: import.meta.env.VITE_Access_Key });
 function Home() {
     const [list, setList]=useState([]);
+    const handleRequest=(data)=>{
+      unsplash.search.getPhotos({
+        query: 'cat',
+        page: 1,
+        perPage: 10,
+        color: 'green',
+        orientation: 'portrait',
+      }).then((res)=>{console.log(JSON.stringify((res.response.results.map((ele)=>ele.links.download)),null,2))
+        setList(res.response.results)
+      }).catch((err)=>{
+        console.log(err.message);
+      })
+    }
     useEffect(()=>{
-        fetch("https://api.unsplash.com/search/collections?page=1&query=office");
+      // handleRequest()
         // https://api.unsplash.com/photos?page=1
+        // const data=
     },[]);
   return (
     <div>
       <h1 className='text-3xl'>Welcome to Image Generator</h1>
+      <Carousel images={data}/>
     </div>
   )
 }
 
-export default Home
+export default Home;
+
