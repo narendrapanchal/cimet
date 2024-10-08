8aa81693156a2ee1820c3f811667c94a6a115b4c
inno <narendrapanchal020@gmail.com>
2024-10-08 10:13:10 +0530

    image-generator

--- image-generator/src/components/Carousel.jsx
+++ image-generator/src/components/Carousel.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useRef, useState } from 'react';
 
 function Carousel({ images, interval }) {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentIndex=useRef(0)
   const [direction, setDirection] = useState('');
   const containerRef = React.useRef(null);
   useEffect(()=>{
+    const timer=setInterval(()=>{
+      console.log("currentIndex,",currentIndex,)
+      handleNext();
+    },2000)
+    return ()=>{
+      clearInterval(timer);
+    }
   },[])
   const handleNext = () => {
-    const imageWidth = containerRef.current; // Get the width of the container
-    console.log("imageWidth",imageWidth)
-      containerRef.current.scrollLeft = imageWidth;
+    const imageWidth = containerRef.current.children[0].offsetWidth;
+    currentIndex.current=(currentIndex.current+1)%images.length
+      containerRef.current.scrollLeft = (currentIndex.current)*imageWidth;
   };
 
   const handlePrevious = () => {
-    const imageWidth = containerRef.current.style.width; // Get the width of the container
-    containerRef.current.scrollRight = imageWidth;
+    const imageWidth =  containerRef.current.children[0].offsetWidth;
+    currentIndex.current=(currentIndex.current-1+images.length)%images.length
+     // Get the width of the container
+    containerRef.current.scrollLeft = currentIndex.current*imageWidth;
   };
 
   return (
-    <div className={`carousel ${direction}`}>
+    <div className={`carousel `}>
       <div ref={containerRef}  style={{display:"flex", overflow:"scroll", flexWrap:"nowrap"}}>
-      {images.map((image,ind)=><img src={image}  alt={"Image "+ind} style={{width:'inherit'}}/>)}
+      {images.map((image,ind)=><img key={"image "+ind} src={image}  alt={"Image "+ind} style={{width:'inherit'}}/>)}
       </div>
       <button onClick={handlePrevious} className='bg-white absolute left-1 top-1/2'>Prev</button>
       <button onClick={handleNext} className='bg-white absolute right-1 top-1/2'>Next</button>
--- image-generator/src/components/Form.jsx
+++ image-generator/src/components/Form.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createApi } from 'unsplash-js';
+const unsplash = createApi({ accessKey: import.meta.env.VITE_Access_Key });
+function Form({setList}) {
+  const [limit,setLimit]=useState(10);
+  const [search,setSearch]=useState("");
+  const [orientation,setOrientation]=useState("landscape");
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(search.length==0){
+      unsplash.photos.getRandom({
+        count: limit,
+        orientation: orientation,
+      }).then((res)=>{
+        setList(res.response.map((ele)=>ele.links.download))
+      })
+    }else{
+      unsplash.search.getPhotos({
+        query: search,
+        page: 1,
+        perPage: limit,
+        orientation: orientation,
+      }).then((res)=>{
+        setList(res.response.results.map((ele)=>ele.links.download))
 
-function Form({handleSubmit}) {
+      }).catch((err)=>{
+        alert("Something went wrong ",err.message)
+      })
+    }
+  }
+
+
   return (
        <form onSubmit={handleSubmit}>
-      <input type="text" placeholder='Search Images' className='border border-teal-900 p-2'/>
+      <input type="text" placeholder='Search Images' className='border border-teal-900 p-2' onChange={(e)=>{
+        setSearch(e.target.value)
+      }}/>
+        <br/>
+      <input required type="number" min={1} max={100} placeholder='Search Images' className='border border-teal-900 p-2' onChange={(e)=>{
+        console.log(e.target.value)
+        setLimit(e.target.value)
+      }}/>
       <br/>
+      <select onChange={(e)=>{
+        setOrientation(e.target.value)
+        console.log(e.target.value)
+      }}>
+        <option value="landscape">Landscape</option>
+        <option value="portrait">Portrait</option>
+        <option value="squarish">Squarish</option>
+      </select>
+      <br/> 
       <button className='mt-2 p-2 font-teal-900 border border-teal-900' type='submit'>Find Images</button>
       </form>
   )
