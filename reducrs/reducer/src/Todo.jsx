732f7378e498f875931db61e58f6667ce7cbe85d
inno <narendrapanchal020@gmail.com>
2024-10-10 10:00:59 +0530

    reducers

---
+++ reducrs/reducer/src/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const initailState={
+    list:[],
+    isEditing:false,
+    currentId:null,
+}
+function Todo() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Todo
